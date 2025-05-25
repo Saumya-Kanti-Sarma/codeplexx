11c45d0c70b3c5f841b23d2cf2620aea49606c22
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-05-25 15:07:58 +0530

    premitive data type removed

--- src/hooks/Truncate.ts
+++ src/hooks/Truncate.ts
@@ -1,3 +1,3 @@
-export const truncateTxt = (text: String, length: number, followUp: string) => {
+export const truncateTxt = (text: string, length: number, followUp: string) => {
   return text.length > length ? `${text.substring(0, length)}${followUp}` : text;
 }
