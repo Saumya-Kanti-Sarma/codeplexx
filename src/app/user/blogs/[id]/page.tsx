6edf8bfa79be5287b1856d411ce67ebeeef6efab
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-06-01 17:30:23 +0530

    added metadatas

--- src/app/user/blogs/[id]/page.tsx
+++ src/app/user/blogs/[id]/page.tsx
@@ -1,12 +1,44 @@
 // app/user/blogs/[id]/page.tsx
 import Blogs from "./Blogs";
+import type { Metadata } from 'next'
 
-export async function generateMetadata() {
+type Props = {
+  params: Promise<{ id: string }>
+}
+export async function generateMetadata(
+  { params }: Props,
+): Promise<Metadata> {
 
+  const id = (await params).id;
+  const data = await fetch(`${process.env.URL}/api/blogs/one?id=${id}`)
+  const response = await data.json();
 
   return {
-    title: "Blogs",
-    description: "this is the blogs section of c plexx",
+    title: response.data[0].title || "Blogs",
+    description: response.data[0].content.substring(0, 60) || "this is the blogs section of c plexx",
+    openGraph: {
+      title: response.data[0].title || "Blogs",
+      description: response.data[0].content.substring(0, 60) || "this is the blogs section of c plexx",
+      url: `${process.env.URL}/user/blogs/${id}`,
+      siteName: 'C Plexx',
+      images: [
+        {
+          url: response.data[0].image_url || '/def.def-img.jpeg',
+          width: 1200,
+          height: 630,
+          alt: response.data[0].title || "Blogs",
+        }
+      ],
+      locale: 'en_US',
+      type: 'article',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: response.data[0].title || "Blogs",
+      description: response.data[0].content.substring(0, 60) || "this is the blogs section of c plexx",
+      images: [response.data[0].image_url || '/def.def-img.jpeg'],
+      creator: response.data[0].uploaded_by || "Codeplexx",
+    },
   };
 }
 
