f0af74ac85bb337a0d163f4c83ba4aad7b577ade
Saumya Kanti Sarma <167862873+Saumya-Kanti-Sarma@users.noreply.github.com>
2025-07-05 20:20:52 +0530

    added app

--- src/app/api/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { withApiKey } from "../libs/middleware/checkApiKey";
-
-async function handler() {
-  return NextResponse.json({
-    message: "ApiKey Validated",
-  }, { status: 200 })
-}
-export const GET = withApiKey(handler);
--- src/hooks/truncate/Truncate.ts
+++
@@ -1,3 +0,0 @@
-export const truncateTxt = (text: string, length: number, followUp: string) => {
-  return text.length > length ? `${text.substring(0, length)}${followUp}` : text;
-}
